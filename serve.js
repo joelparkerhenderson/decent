ff6e9ee5d67aea0fcfa2b2cba9ec20d7db52509f
Ev Bogue <ev@evbogue.com>
2017-11-16 11:28:06 -0600

    make decent compile with pkg -- also launch decent in browser

--- serve.js
+++ serve.js
@@ -1,8 +1,12 @@
-var http = require('http');
-var serve = require('ecstatic');
+var http = require('http')
+var serve = require('ecstatic')
+var open = require('opener')
+
+var PORT = 3001
 
 exports.init = function() {
   http.createServer(
     serve({ root: __dirname + '/build/'})
-  ).listen(3001)
+  ).listen(PORT)
+  open('http://localhost:3001')
 }
