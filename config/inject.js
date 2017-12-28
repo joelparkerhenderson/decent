956ea290655c84d90af464bbd24feb73f868cc91
Ev Bogue <ev@evbogue.com>
2017-12-28 10:32:31 -0600

    configure network based on network name

--- config/inject.js
+++ config/inject.js
@@ -14,26 +14,64 @@ var SEC = 1e3
 var MIN = 60*SEC
 
 module.exports = function (name, override) {
-  //name = name || 'ssb'
+  //uncomment the network name, and equivilent config, you want to use before building and starting decent. To generate a new caps use `> crypto.randomBytes(32).toString('base64')` in your node repl
+
   name = name || 'decent'
+  //name = name || 'ssb'
+  //name = name || 'testnet'
+
+  var network
+
+  if (name === 'ssb') {
+    network = {
+      port: 8008,
+      ws: {
+        port: 8989
+      },
+      caps: {
+        shs: '1KHLiKZvAvjbY1ziZEHMXawbCEIM6qwjCDm3VYRan/s=',
+        sign: null
+      }
+    }
+  }
+
+  if (name === 'decent') {
+    network = {
+      port: 3333,
+      ws: {
+        port: 3939,
+        remote: 'ws://decent.evbogue.com:3939~shs:hvITDG3iM3iJEy91XmlBKKf4VHgzU+0LLH/VD+AVhOE='
+      }, 
+      caps: { 
+        shs: 'EVRctE2Iv8GrO/BpQCF34e2FMPsDJot9x0j846LjVtc=',
+        sign: null 
+      }
+    }
+  }
+
+  if (name === 'testnet') {
+    network = {   
+      port: 3333,
+      ws: {
+        port: 3939
+      }, 
+      caps: {
+        shs: 'sR74I0+OW6LBYraQQ2YtFtqV5Ns77Tv5DyMfyWbrlpI=',
+        sign: null  
+      }
+    }
+  }
+  
   var HOME = home() || 'browser' //most probably browser
-  return RC(name, merge({
-    //just use an ipv4 address by default.
-    //there have been some reports of seemingly non-private
-    //ipv6 addresses being returned and not working.
-    //https://github.com/ssbc/scuttlebot/pull/102
+  return RC(name, merge(network, {
+     //standard stuff that probably doesn't need to change below
     host: nonPrivate.v4 || '',
-    port: 3333,
     timeout: 0,
     pub: true,
     local: true,
     friends: {
       dunbar: 150,
       hops: 3
-    },
-    ws: {
-      port: 3939,
-      remote: 'ws://decent.evbogue.com:3939~shs:hvITDG3iM3iJEy91XmlBKKf4VHgzU+0LLH/VD+AVhOE='
     },
     gossip: {
       connections: 3
@@ -44,11 +82,6 @@ module.exports = function (name, override) {
       reconnect: 5*SEC,
       ping: 5*MIN,
       handshake: 5*SEC
-    },
-    caps: { 
-      shs: 'EVRctE2Iv8GrO/BpQCF34e2FMPsDJot9x0j846LjVtc=',
-      //shs: '1KHLiKZvAvjbY1ziZEHMXawbCEIM6qwjCDm3VYRan/s=',
-      sign: null
     },
     master: [],
     party: true //disable quotas
