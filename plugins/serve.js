0cd3ce843c00b8b2c370047b03ffd9a62fe1a4fe
Ev Bogue <ev@evbogue.com>
2018-01-25 09:06:08 -0600

    move ws address server into the build server

--- plugins/serve.js
+++ plugins/serve.js
@@ -2,11 +2,20 @@ var http = require('http')
 var serve = require('ecstatic')
 var open = require('opener')
 
-var PORT = 3001
+exports.name = 'serve'
+exports.version = '1.0.0'
 
-exports.init = function() {
-  http.createServer(
+exports.init = function(sbot) {
+  http.createServer( 
     serve({ root: __dirname + '/build/'})
-  ).listen(PORT)
+  ).listen(3001)
+
   open('http://localhost:3001')
+
+  sbot.ws.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    if(req.url == '/get-address')
+      res.end(sbot.ws.getAddress())
+    else next()
+  })
 }
