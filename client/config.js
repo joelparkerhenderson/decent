1cf511f8031aaf7dd56124882c1d4cf224e4d959
Ev Bogue <ev@evbogue.com>
2018-02-07 12:18:44 -0600

    trim unnecessary code

--- client/config.js
+++ client/config.js
@@ -1,48 +1,20 @@
-var config = require('./../config/inject.js')(process.env.ssb_appname)
-
 var http = require('http')
 var URL = require('url')
 
 module.exports = function () {
 
-  // set a default ws.remote if there is none, overrideable in the client
-   if ((localStorage[config.path + '/remote'] === undefined) || (localStorage[config.path + '/remote'] === '')) {
-    http.get('http://localhost:'+ config.ws.port + '/get-address', function (res) {
-      res.on('data', (ws) => {
-        ws = ws + ''
-        if (ws.startsWith('ws://')) {
-          localStorage[config.path + '/remote'] = ws
-        } else {
-          console.log(ws + 'is not a valid ws address')
-        }
-      })
-    }).on('error', (e) => {
-      console.log(e.message)
-      localStorage[config.path + '/remote'] = config.ws.remote
+  var host = window.location.origin
+
+  http.get(host + '/get-config', function (res) {
+    res.on('data', function (data, remote) {
+      config = data
+      localStorage[host] = config
     })
-  }
- 
-  var remote = 'undefined' === typeof localStorage
-    ? null //'ws://localhost:8989~shs:' + require('./keys')
-    : localStorage[config.path + '/remote']
+  })
 
-  //var remote = config.ws.remote
-
-  //TODO: use _several_ remotes, so if one goes down,
-  //      you can still communicate via another...
-  //      also, if a blob does not load, use another pub...
-
-  //if we are the light client, get our blobs from the same domain.
-  var blobsUrl
-  if(remote) {
-    var r = URL.parse(remote.split('~')[0])
-    //this will work for ws and wss.
-    r.protocol = r.protocol.replace('ws', 'http')
-    r.pathname = '/blobs/get'
-    blobsUrl = URL.format(r)
-  }
-  else
-    blobsUrl = 'http://localhost:8989/blobs/get'
+  var config = JSON.parse(localStorage[host])
+  var remote = config.address
+  var blobsUrl = host + '/blobs/get'
 
   return {
     remote: remote,
